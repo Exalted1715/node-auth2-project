b4bd8cd3abbb889d6cce81ddc3632c950de9f586
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-05-02 12:47:03 -0600

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -42,7 +42,12 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
- next()
+ 
+ if(role_name === req.decodedToken.role_name){
+  next()
+ } else{
+  next({status:403, message: 'This is not for you'})
+ }
 }
 
 
@@ -57,7 +62,7 @@ const checkUsernameExists = async (req, res, next) => {
     try{
       const [user] = await findBy({username: req.body.username})
       if(!user){
-        next({status: 422, message: 'Invalid credentials'})
+        next({status: 401, message: 'Invalid credentials'})
       } else{
         req.user = user
         next()
@@ -87,18 +92,18 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
- if(!req.body.role_name || !req.body.role_name.trim()){
-  req.role_name = 'student'
-  next()
- } else if(req.body.role_name.trim() === ' admin') {
-  next({status:422, message: 'Role name can not be admin'})
- } else if(req.body.role_name.trim().length > 32) {
-  next({status:422, message: 'Rolename can not be longer than 32 chars'})
- } else{
-  req.role_name = req.body.role_name.trim()
-  next()
- }
-}
+    if(!req.body.role_name || !req.body.role_name.trim()){
+      req.role_name = 'student'
+      next()
+     } else if(req.body.role_name.trim() === 'admin') {
+      next({status:422, message: 'Role name can not be admin'})
+     } else if(req.body.role_name.trim().length > 32) {
+      next({status:422, message: 'Rolename can not be longer than 32 chars'})
+     } else{
+      req.role_name = req.body.role_name.trim()
+      next()
+     }
+    }
 
 module.exports = {
   restricted,
